1772b538983de21dfb80cf4fdc42c2d9d6f1f75f
omkarhadole <omkarhadole38@gmail.com>
2026-01-26 15:03:20 +0530

    fix login CORS issue

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,7 @@ const cache = new NodeCache({ stdTTL: 600 });
 
 app.use(compression());
 
-app.use(cors());
-
+// app.use(cors());
 
 // app.use(cors({
 //   origin: 'http://localhost:5173',
@@ -30,6 +29,25 @@ app.use(cors());
 //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 //   allowedHeaders: ['Content-Type', 'Authorization']
 // }));
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://zenova-x.vercel.app',
+  'https://zenova-x-server.vercel.app'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 app.use((req, res, next) => {
