562607d87e32564f46c36cb61c82a0334995b921
omkarhadole <omkarhadole38@gmail.com>
2026-02-10 17:46:29 +0530

    fix login issue

--- backend/server.js
+++ backend/server.js
@@ -22,21 +22,11 @@ const cache = new NodeCache({ stdTTL: 600 });
 
 app.use(compression());
 
-app.use(cors());
-
-// app.use(cors({
-//   origin: 'http://localhost:5173',
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
-
-// const allowedOrigins = [
-//   'http://localhost:5173',
-//   'https://zenova-x.vercel.app',
-//   'https://zenova-x-server.vercel.app'
-// ];
-
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://zenova-x.vercel.app',
+  'https://zenova-x-server.vercel.app'
+];
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
